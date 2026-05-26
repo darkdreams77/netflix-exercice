b1d3de00ce23ed49010fc4ac48abf6052a491016
darkdreams77 <darkdreams77@users.noreply.github.com>
2026-05-26 10:54:16 +0200

    feat(app): update logo and dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import "./App.css";
 import data from "./assets/data.json";
 import { Section } from "./components/Section";
 
+import logo from "./assets/logo.png";
+
 function App() {
   return (
     <>
       <header>
         <a href="/">
-          <img src="/src/assets/logo.png" alt="Netflix" className="logo" />
+          <img src={logo} alt="Netflix" className="logo" />
         </a>
       </header>
-      {data.map((el) => (
-        <Section title={el.category} images={el.images} />
-      ))}
+      <main>
+        {data.map((el) => (
+          <Section title={el.category} images={el.images} />
+        ))}
+      </main>
     </>
   );
 }
